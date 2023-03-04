513fcab257dafc21172a9cfd500d6663419887ac
Fernando Navarro <fernandonavarro@berkeley.edu>
2023-03-03 19:26:28 -0800

    Timer and interval implemented
    getCurrentTime() with setInterval every second

--- index.js
+++ index.js
@@ -36,3 +36,13 @@ fetch('https://api.coingecko.com/api/v3/coins/frax-share')
         `;
   })
   .catch((err) => console.error(err));
+
+function getCurrentTime() {
+  const date = new Date();
+  document.getElementById('time').textContent = date.toLocaleTimeString(
+    'en-us',
+    { timeStyle: 'short' }
+  );
+}
+
+setInterval(getCurrentTime, 1000);
